19555efddbca714193044c8992ebb1f6f63ca732
H Lenin <h.lenin@gmail.com>
2021-09-29 21:15:17 -0300

    criação de api e configuração de variável de ambiente

---
+++ pages/api/filme.js
@@ -0,0 +1,16 @@
+async function filme(request, response) {
+    const apiToken = process.env.THE_MOVIE_DB_API_TOKEN;
+    const dynamicDate = new Date();
+
+    const movieID = "343611";
+    const movieResponse = await fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=${apiToken}`);
+    const movieResponseJson = await movieResponse.json();
+    const movieOverview = movieResponseJson.overview;
+
+    response.json({
+        date: dynamicDate.toGMTString(),
+        sinopse: movieOverview
+    });
+}
+
+export default filme;
