5bdf13898e314eb328ef279698083ba59ff725f3
H Lenin <h.lenin@gmail.com>
2021-09-30 08:12:15 -0300

    api geração de id aleatório para filme

--- pages/api/filme.js
+++ pages/api/filme.js
@@ -2,16 +2,35 @@ async function Filme(request, response) {
     const apiToken = process.env.THE_MOVIE_DB_API_TOKEN;
     const dynamicDate = new Date();
 
-    const movieID = "343611";
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    const movieID = getRndInteger(55555, 300000);
     const movieResponse = await fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=${apiToken}`);
     const movieResponseJson = await movieResponse.json();
+    const movieTitle = movieResponseJson.title;
+    const movieGenre = "no genre info available";
+    try {
+        const movieGenre = movieResponseJson.genres[0].name;
+    } catch (error) {
+
+    }
+
+    const movieRuntime = movieResponseJson.runtime;
     const movieOverview = movieResponseJson.overview;
+
 
     response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
 
     response.json({
         date: dynamicDate.toGMTString(),
-        sinopse: movieOverview
+        movieId: movieID,
+        title: movieTitle,
+        genre: movieGenre,
+        runtime: movieRuntime,
+        sinopse: movieOverview,
+        all: movieResponseJson
     });
 }
 
