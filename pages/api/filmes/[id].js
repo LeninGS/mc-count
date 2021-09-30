c301e6392691b9c46a719d8f968650ac85905ac2
H Lenin <h.lenin@gmail.com>
2021-09-30 12:35:16 -0300

    adequação da API para o padrão Rest

--- pages/api/filmes/[id].js
+++ pages/api/filmes/[id].js
@@ -1,3 +1,40 @@
-export default function GetFilmeInfos(request, response) {
-    return request.query.id;
+export default async function GetFilmeInfos(request, response) {
+    const movieID = request.query.id;
+    const apiToken = process.env.THE_MOVIE_DB_API_TOKEN;
+    const dynamicDate = new Date();
+
+    const movieResponse = await fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=${apiToken}`);
+    const movieResponseJson = await movieResponse.json();
+    const movieTitle = movieResponseJson.title;
+    const movieGenre = "";
+    try {
+        const movieGenre = movieResponseJson.genres[0].name;
+    } finally {
+        const movieGenre = "no genre info available";
+    };
+
+    const movieRuntime = movieResponseJson.runtime;
+    const movieOverview = movieResponseJson.overview;
+
+
+    response.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
+
+    response.json({
+        date: dynamicDate.toGMTString(),
+        movieId: movieID,
+        title: movieTitle,
+        genre: movieGenre,
+        runtime: movieRuntime,
+        sinopse: movieOverview,
+        all: movieResponseJson
+    });
+
+    response.json({
+        date: dynamicDate.toGMTString(),
+        movieId: movieID,
+        title: movieTitle,
+        genre: movieGenre,
+        runtime: movieRuntime,
+        sinopse: movieOverview
+    });
 }
