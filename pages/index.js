c05b2f919e4ec1886f861a53ceac9858e1beaf15
H Lenin <h.lenin@gmail.com>
2021-10-03 03:03:20 -0300

    implementação de animações, ajustes de layout e adição dos primeiros itens

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 function HomeOld() {
     return (<div>
@@ -34,41 +35,84 @@ function Counter() {
     );
 }
 
-export default function Home() {
+export default function Home(props) {
     const [pickaxe, setPickaxe] = useState("/wooden_pickaxe.png");
 
+    const [clickCount, setClickCount] = useState(0);
+
+    function showClickingText() {
+        if (clickCount > 0 && clickCount == 1) {
+            return `Você clicou ${clickCount} vez.`;
+        } else if (clickCount > 0) {
+            return `Você clicou ${clickCount} vezes.`;
+        }
+    }
+
+    function changeBackground(e) {
+        e.target.style.background = 'red';
+    }
+
+    function changeBackground2(e) {
+        e.target.style.background = 'blue';
+    }
+
     return (
-        <body>
-            <div class="hero">
-                <div class="navbar">
-                    <img src="/Set_your_amount.gif" class="logo" name="logo"></img>
-                    <Link href="/">
-                        <button type="button" class="button" name="home">Home</button>
-                    </Link>
-                    <button type="button" class="button" name="about">About</button>
-                </div>
-                <div class="content">
-                    <small>Welcome to</small>
-                    <h1>Mine Count</h1>
-                    <h3>Coming soon</h3>
-                    <div class="mining">
-                        <span class="cobblestone">
-                            <img src="/cobblestone.png"></img>
+        <>
+            <body>
+                <div class="hero">
+                    <div class="navbar">
+                        <img src="/Set_your_amount.gif" class="logo" name="logo"></img>
+                        <Link href="/">
+                            <button type="button" class="button" name="home">Home</button>
+                        </Link>
+                        <button type="button" class="button" name="about">About</button>
+                    </div>
+                    <div class="content">
+                        <small>Welcome to</small>
+                        <h1>Mine Count</h1>
+                        <span>
+                            <h3>Coming soon</h3>
                         </span>
-                        <span class="pickaxe">
-                            <img id="pickaxe" src={pickaxe}></img>
-                        </span>
+                        <div class="mining">
+                            <span class="cobblestone">
+                                <img src="/cobblestone.png" onClick={() => setClickCount(clickCount + 1)}></img>
+                            </span>
+                            <span class="pickaxe">
+                                <img id="pickaxe" src={pickaxe}></img>
+                            </span>
+                            <span class='text-clicking'>
+                                {showClickingText()}
+                            </span>
+                        </div>
+                        <div class="samples">
+                            <div>
+                                <img src="/Set_your_amount.gif"></img>
+                                <div>1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod massa ac arcu tempor vehicula. Donec efficitur pulvinar ipsum, ut auctor enim feugiat sit amet. Duis ut turpis at erat dignissim feugiat a eu ipsum. Nulla elementum, tellus et pretium egestas, risus est posuere enim, sit amet faucibus tellus augue eget risus.</div>
+                            </div>
+                            <div>
+                                <img src="/Set_your_amount.gif"></img>
+                                <div>2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod massa ac arcu tempor vehicula. Donec efficitur pulvinar ipsum, ut auctor enim feugiat sit amet. Duis ut turpis at erat dignissim feugiat a eu ipsum. Nulla elementum, tellus et pretium egestas, risus est posuere enim, sit amet faucibus tellus augue eget risus.</div>
+                            </div>
+                            <div>
+                                <img src="/Set_your_amount.gif"></img>
+                                <div>3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod massa ac arcu tempor vehicula. Donec efficitur pulvinar ipsum, ut auctor enim feugiat sit amet. Duis ut turpis at erat dignissim feugiat a eu ipsum. Nulla elementum, tellus et pretium egestas, risus est posuere enim, sit amet faucibus tellus augue eget risus.</div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="side-bar">
+                        <div class="menu-items">
+                            <img src="/chest.gif"></img>
+                            <img src="/diamond.png" onClick={() => setPickaxe("/diamond_pickaxe.png")}></img>
+                            <img src="/gold_ingot.png" onClick={() => setPickaxe("/golden_pickaxe.png")}></img>
+                            <img src="/iron_ingot.png" onClick={() => setPickaxe("/iron_pickaxe.png")}></img>
+                        </div>
                     </div>
                 </div>
-                <div class="side-bar">
-                    <div class="menu-items">
-                        <img src="/chest.gif"></img>
-                        <img src="/diamond.png" onClick={() => setPickaxe("diamond_pickaxe.png")}></img>
-                        <img src="/gold_ingot.png" onClick={() => setPickaxe("/golden_pickaxe.png")}></img>
-                        <img src="/iron_ingot.png" onClick={() => setPickaxe("/iron_pickaxe.png")}></img>
-                    </div>
-                </div>
-            </div>
-        </body >
+            </body>
+            <script>
+
+            </script>
+        </>
     )
 };
